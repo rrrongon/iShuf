5d8b5743d9bea24ff36092ce3ac7a4e92279ef5c
rongon <r.rongon@wsu.edu>
2023-02-22 18:12:30 -0800

    -datasplit

--- pytorch_imagenet_resnet50.py
+++ pytorch_imagenet_resnet50.py
@@ -7,8 +7,10 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torchvision import transforms, datasets
 import numpy as np
+from utility.sampler import DistributedSampler as dsampler
 
 import json
+from utility._dataSplit import split_data
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Example',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -27,6 +29,10 @@ if __name__ == '__main__':
     print("Is CUDA available? - :" + str(args.cuda))
 
     hvd.init()
+    if args.cuda:
+        torch.cuda.set_device(hvd.local_rank())
+        torch.cuda.manual_seed(args.seed)
+        print("local rank: {0}", hvd.local_rank())
     rank = hvd.rank()
     print ("Run with arguments:") if hvd.rank() == 0 else None
     for key, value in args._get_kwargs():
@@ -38,6 +44,12 @@ if __name__ == '__main__':
     configs =json.load(f)
     root_dir = configs["ROOT_DATADIR"]["dir"]
 
+    #model param configs
+    model_configs = configs["MODEL"]
+    batch_size = model_configs['batch_size']
+    batches_per_allreduce = model_configs['batches_per_allreduce']
+    allreduce_batch_size = batch_size * batches_per_allreduce
+
     print("root dir: {0}", root_dir)
 
     image_transforms = {
@@ -48,13 +60,36 @@ if __name__ == '__main__':
     }
 
     #Creating own dataset
-    natural_img_dataset = datasets.ImageFolder(
-                                root = root_dir,
-                                transform = image_transforms["train"]
-                        )
+    #natural_img_dataset = datasets.ImageFolder(
+    #                            root = root_dir,
+    #                            transform = image_transforms["train"]
+    #                    )
 
     np.random.seed(0)
     torch.manual_seed(0)
     #train_dataset, val_dataset = random_split(natural_img_dataset, (6000, 899))
 
-    print("from rank: {0}: {1}", rank, natural_img_dataset.class_to_idx)
+    sp = split_data()
+    sp.split()
+    train_folder, val_folder = sp._get_folders()
+
+    train_dataset = datasets.ImageFolder(
+                                root = train_folder,
+                                transform = image_transforms["train"]
+                        )
+    
+    val_dataset = datasets.ImageFolder(
+                                root = val_folder,
+                                transform = image_transforms["train"]
+                        )
+
+    train_sampler = dsampler(
+        train_dataset, num_replicas=hvd.size(), rank=hvd.rank())
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=allreduce_batch_size,
+        sampler=train_sampler)
+    
+    for tensor in train_loader:
+        print(tensor)
+        print(train_dataset.class_to_idx)
+        break
---
+++ utility/_dataSplit.py
@@ -0,0 +1,113 @@
+import torch
+import horovod.torch as hvd
+import argparse, os
+import math
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import transforms, datasets
+import numpy as np
+import json
+import random
+import shutil
+
+THRESH_HOLD = 5
+IMG_EXTENSIONS = [
+    '.jpg', '.JPG', '.jpeg', '.JPEG',
+    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+]
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+def copy_dataset_class(in_folder, out_folder,filenames): #, f):
+    if not os.path.exists(out_folder):
+        os.mkdir(out_folder)
+        
+    for filename in filenames:
+        source_file_path = os.path.join(in_folder,filename)
+        dest_file_path = os.path.join(out_folder,filename)
+        command = "cp " + str(source_file_path) + " " + str(dest_file_path)
+        #print("command: {0}".format(command))
+        #print("Source file: {0}\n dest file: {1}".format(source_file_path, dest_file_path))
+        os.system(command)
+
+class split_data:
+    def __init__(self, val_parcent=20):
+        self.val_parcent = val_parcent
+
+        f = open('config.json')
+        configs =json.load(f)
+        self.root_dir = configs["ROOT_DATADIR"]["dir"]
+        self.source_data_dir = configs["ROOT_DATADIR"]["data_dir"]
+
+        self.target_folder = os.path.join(self.root_dir,'data_2')
+        if not os.path.exists(self.target_folder):
+            os.mkdir(self.target_folder)
+        self.train_data_dir = os.path.join(self.target_folder, 'train')
+        self.val_data_dir = os.path.join(self.target_folder, 'val')
+
+        if not os.path.exists(self.train_data_dir):
+            os.mkdir(self.train_data_dir)
+        if not os.path.exists(self.val_data_dir):
+            os.mkdir(self.val_data_dir)
+    
+    def split(self):
+        classes = []
+        imgs = []
+        total_samples = 0
+
+        all_classes = os.listdir(self.source_data_dir)
+        print(len(all_classes))
+
+        all_classes.sort()
+        for _class in all_classes:
+            d = os.path.join(self.source_data_dir, _class)
+            if not os.path.isdir(d):
+                print(d,"is not dir")
+                continue
+
+            filenames = []
+            for filename in os.listdir(d):
+                if is_image_file(filename):
+                    path = '{0}/{1}'.format(_class, filename)
+                    item = (path, _class)
+                    #filenames.append(item)
+                    filenames.append(path)
+                
+            if len(filenames) >= THRESH_HOLD:
+                total_samples = total_samples + len(filenames)
+                classes.append((_class,len(filenames)))
+                random.seed(230)
+                filenames.sort()
+                random.shuffle(filenames)
+                split = int( (1-self.val_parcent*0.01) * len(filenames))
+                train_filenames = filenames[:split]
+                val_filenames = filenames[split:]
+
+                #print("train file len {0} and val file len {1}".format(len(train_filenames), len(val_filenames)))
+                try:
+                    train_sub_folder = os.path.join(self.train_data_dir, _class)
+                    if not os.path.exists(train_sub_folder):
+                        os.mkdir(train_sub_folder)
+                    val_sub_folder =  os.path.join(self.val_data_dir, _class)
+                    if not os.path.exists(val_sub_folder):
+                        os.mkdir(val_sub_folder)
+
+                    copy_dataset_class(self.source_data_dir,self.train_data_dir, train_filenames)#,f1)
+                    # for filename in train_filenames:
+                    # f1.write(str(filename) + "\n")
+                    copy_dataset_class(self.source_data_dir,self.val_data_dir, val_filenames) #,f2)
+                    
+                except:
+                    print("len of traing files: {0}, val files{1}".format(len(train_filenames), len(val_filenames)))
+                    print("Exception with class",_class)
+                    shutil.rmtree(self.data_folder)
+            else:
+                print("Remove class",_class, "with",len(filenames),"samples")
+
+    def _get_folders(self):
+        return self.train_data_dir, self.val_data_dir
+if __name__ == '__main__':
+    sp = split_data()
+    sp.split()
+    sp._get_folders()
