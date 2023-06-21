- accuracy calculation fix

dataloader_test.py
@@ -57,10 +57,10 @@ def custom_accuracy(output, target):
     #print("column max#{0}\n target column max#{1}".format(pred_column_max, target_column_max))
     correct = (pred_column_max == target_column_max).sum().item()
     total = target.size(0)
-    accuracy = correct / total
+    #accuracy = correct / total
 
     #print("column max#{0}\n correct#{1}\n accuracy#{2}".format(pred_column_max, correct, accuracy))
-    return accuracy * 100
+    return correct, total
 
 
 class Result(object):
@@ -93,7 +93,7 @@ def plot_timeBreakdown(epochs, plt_comp_time, plt_reading_time, plt_shuffling_ti
     plt.xticks(range(epochs))
     plt.ylabel('Time (seconds)')
     plt.title('Training Time breakdown per epoch: PARTIAL')
-    plt.legend(['Reading time', 'Shuffling time', 'Computation time', 'Total time'], loc='upper left')
+    plt.legend(['Reading time', 'Shuffling time', 'Computation time', 'Total time'], loc='upper right')
 
     # Show the plot
     plt.savefig('Imagenet_training_timeBreakdown_PARTIAL.png')
@@ -131,7 +131,7 @@ def train(epoch, mini_batch_limit, nc):
     loss_onIndex_onEpoch = dict()
 
     nc._set_current_epoch(epoch) # IS
-    
+
     rank = hvd.rank()
     world_size = hvd.size()
 
@@ -139,19 +139,19 @@ def train(epoch, mini_batch_limit, nc):
     loss_res = Result("Loss")
 
     model.train()
-    _batch_size = configs["MODEL"]["batch_size"] 
+    _batch_size = configs["MODEL"]["batch_size"]
     _validation_iteration_number = 7
 
     # Check if GPU is available
     if torch.cuda.is_available():
         device = torch.device("cuda")
     else:
-        device = torch.device("cpu")   
+        device = torch.device("cpu")
 
 
     #Inter process communication initialization
     nc.scheduling(epoch)
-    
+
     data_loading_times = list()
     data_loading_start_time = time.time()
 
@@ -165,7 +165,10 @@ def train(epoch, mini_batch_limit, nc):
     total_comp_loss_time = 0
     total_comp_backward_time = 0
     total_comp_loss_div_time = 0
-    
+
+    total_correct=0
+    total_sample = 0
+
     for batch_idx, (data, target, path, index_list_tensor) in enumerate(_train_loader):
 
         if batch_idx < mini_batch_limit-1:
@@ -178,7 +181,7 @@ def train(epoch, mini_batch_limit, nc):
             if _is_cuda:
                 data, target = data.cuda(), target.cuda()
 
-            print("Rank#{0}, Epoch#{1}, Mini-batch#{2} Time to read mini-batch: {3} seconds".format(rank, epoch, batch_idx, (loading_end_time - loading_start_time)))
+            #print("Rank#{0}, Epoch#{1}, Mini-batch#{2} Time to read mini-batch: {3} seconds".format(rank, epoch, batch_idx, (loading_end_time - loading_start_time)))
 
             time_allreduce = hvd.allreduce(torch.tensor(loading_end_time - loading_start_time), average=True)
             total_reading_time += time_allreduce.item()
@@ -196,14 +199,14 @@ def train(epoch, mini_batch_limit, nc):
             if len(train_data_splits) > 0:
                 cnt = 0
                 for i in range(len(train_data_splits)):
-                
+
                     process_train_data = train_data_splits[i] #data to train for ranks
                     process_train_target = train_target_splits[i] #label of those data
 
                     #tackle last mini batch
                     if len(process_train_data) != _batch_size:
                         continue #currently skipping last batch. Later tackle using proper method
-            
+
                     #check length of data and target are same
                     assert len(process_train_data) == len(process_train_target) , "Error in splitting of data and target "
 
@@ -211,7 +214,7 @@ def train(epoch, mini_batch_limit, nc):
                     computation_output_start_time = time.time()
                     output = model(process_train_data)
                     computation_output_end_time = time.time()
-                    
+
                     comp_output_time_allreduce = hvd.allreduce(torch.tensor(computation_output_end_time - computation_output_start_time), average=True)
                     total_comp_output_time += comp_output_time_allreduce.item()
 
@@ -247,25 +250,24 @@ def train(epoch, mini_batch_limit, nc):
                     computation_time_allreduce = hvd.allreduce(torch.tensor(computation_end_time - computation_start_time), average=True)
                     total_computation_time += computation_time_allreduce.item()
 
-                    acc = custom_accuracy(output, process_train_target)
-      
-                    acc_res.update(acc)
-                    loss_res.update(loss.item())
+                    correct, mini_batch_total = custom_accuracy(output, process_train_target)
+                    total_correct += correct
+                    total_sample += mini_batch_total
+
+                   # acc_res.update(acc)
+                   # loss_res.update(loss.item())
 
         else:
             break
-        print(f"Epoch#{epoch}: Rank#{rank} accuracy#{acc} Percent and loss#{loss.item()}")
-        print(f"Average Rank#{rank} accuracy#{acc_res.avg()} Percent and loss#{loss_res.avg()}")
-        sys.stdout.flush()
 
         # update model parameters
         hvd.barrier()
         optimizer.step()
         torch.cuda.synchronize()
 
-        plt_train_acc.append(acc)
-        plt_train_loss.append(loss.item())
-        
+        #plt_train_acc.append(acc)
+        #plt_train_loss.append(loss.item())
+
         loading_start_time = time.time()
 
     if rank ==0:
@@ -283,19 +285,19 @@ def train(epoch, mini_batch_limit, nc):
     hvd.allreduce(torch.tensor(0), name="barrier")
 
     nc.sync_recv()
-    print("Rank#{0} is done in sync recv".format(rank))
+    #print("Rank#{0} is done in sync recv".format(rank))
     sys.stdout.flush()
     torch.cuda.synchronize()
     hvd.allreduce(torch.tensor(0), name="barrier")
 
     nc.sync_send()
-    print("Rank#{0} is done in sync send".format(rank))
+    #print("Rank#{0} is done in sync send".format(rank))
     sys.stdout.flush()
     torch.cuda.synchronize()
     hvd.allreduce(torch.tensor(0), name="barrier")
 
     nc.clean_sent_samples()
-    print("Rank#{0} is done in clean sent samples".format(rank))
+    #print("Rank#{0} is done in clean sent samples".format(rank))
     sys.stdout.flush()
     torch.cuda.synchronize()
     hvd.allreduce(torch.tensor(0), name="barrier")
@@ -303,7 +305,21 @@ def train(epoch, mini_batch_limit, nc):
     shuffling_end_time = time.time()
     shuffling_time_allreduce = hvd.allreduce(torch.tensor(shuffling_end_time - shuffling_start_time), average=True)
 
+    #correct = custom_accuracy(output, process_train_target)
+    acc = (total_correct/total_sample) * 100
+    acc_res.update(acc)
+    loss_res.update(loss.item())
+
+    plt_train_acc.append(acc)
+    plt_train_loss.append(loss.item())
+
+    print(f"Epoch#{epoch}: Rank#{rank} accuracy#{acc} Percent and loss#{loss.item()}")
+    sys.stdout.flush()
+
     if rank==0:
+        print(f"Average Rank#{rank} accuracy#{acc_res.avg()} Percent and loss#{loss_res.avg()}")
+        sys.stdout.flush()
+
         print("Rank#{0}, Epoch#{1}, Shuffling time: {2} seconds".format(rank, epoch, (shuffling_time_allreduce.item())))
         plt_shuffling_time.append(shuffling_time_allreduce.item())
         plt_comp_time.append(total_computation_time)
@@ -325,25 +341,36 @@ def validation(epoch):
     val_loss = 0.0
     val_acc = 0.0
 
+    total_correct = 0
+    total_sample = 0
     with torch.no_grad():
         for (val_data, val_target, path, index_tensor) in _val_loader:
             val_data, val_target = val_data.cuda(), val_target.cuda()
             val_output = model(val_data)
-            val_acc = custom_accuracy(val_output, val_target)
             val_loss = loss_fn(val_output, val_target)
+            corr, samples = custom_accuracy(val_output, val_target)
+            total_correct += corr
+            total_sample += samples
 
-            val_acc_met.update(val_acc)
-            val_loss_met.update(val_loss.item())
+        val_acc = (total_correct/total_sample ) * 100
+        val_acc_met.update(val_acc)
+        val_loss_met.update(val_loss.item())
 
-            if rank==0:
-                print("````````````````````````````````````````````````")
-                print(f"Epoch# {epoch}: Validatio acc#{val_acc} Percent and val loss#{val_loss.item()}")
-                print(f"Average Validation Loss: {val_loss_met.avg()}, Validation Accuracy: {val_acc_met.avg()} percent")
-                print("`````````````````````````````````````````````````````````````````````")
-                sys.stdout.flush()
+        print("````````````````````````````````````````````````")
+        print(f"Epoch# {epoch}: Validatio acc#{val_acc} Percent and val loss#{val_loss.item()}")
+        print(f"Average Validation Loss: {val_loss_met.avg()}, Validation Accuracy: {val_acc_met.avg()} percent")
+        print("`````````````````````````````````````````````````````````````````````")
+        sys.stdout.flush()
 
-                plt_val_acc.append(val_acc)
-                plt_val_loss.append(val_loss.item())
+        if rank==0:
+            print("````````````````````````````````````````````````")
+            print(f"Epoch# {epoch}: Validatio acc#{val_acc} Percent and val loss#{val_loss.item()}")
+            print(f"Average Validation Loss: {val_loss_met.avg()}, Validation Accuracy: {val_acc_met.avg()} percent")
+            print("`````````````````````````````````````````````````````````````````````")
+            sys.stdout.flush()
+
+            plt_val_acc.append(val_acc)
+            plt_val_loss.append(val_loss.item())
 
         model.train()
 
@@ -365,7 +392,7 @@ if __name__ == '__main__':
         IMGNET_DIR = configs["ROOT_DATADIR"]["imgnet_dir"]
     elif DATASET == _21K:
         IMGNET_DIR = configs["ROOT_DATADIR"]["imgnet_21k_dir"]
-    
+
     train_folder = os.path.join(IMGNET_DIR,"parition" + str(rank)+"/train")
     wnids_file = os.path.join(IMGNET_DIR,"parition" + str(rank)+"/wnids.txt")
     words_file = os.path.join(IMGNET_DIR,"parition" + str(rank)+"/words.txt")
@@ -383,7 +410,7 @@ if __name__ == '__main__':
 
     mini_batch_limit = (min_train_dataset_len / _batch_size)
     print("Rank#{0}: minimum batch number limit#{1}".format(rank,mini_batch_limit))
-    
+
     #custom_sampler = CustomSampler(_train_dataset)
     _train_sampler = dsampler(
             train_dataset, num_replicas=hvd.size(), rank=hvd.rank(), shuffle=True)
@@ -392,7 +419,7 @@ if __name__ == '__main__':
             sampler= _train_sampler)
 
     _val_folder = os.path.join(IMGNET_DIR,"parition" + str(rank)+"/val")
-    _val_dataset = ImageNetDataset(_val_folder, wnids_file, words_file, CLASS_NUMBER, transform=None) 
+    _val_dataset = ImageNetDataset(_val_folder, wnids_file, words_file, CLASS_NUMBER, transform=None)
 
     #custom_sampler = CustomSampler(_val_dataset)
     _val_sampler = dsampler(
@@ -411,7 +438,7 @@ if __name__ == '__main__':
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, num_classes)
 
-    base_lr = 0.0125
+    base_lr = 0.00125
     momentum = 0.9
     scaled_lr = base_lr * hvd.size()
     if _is_cuda:
@@ -435,9 +462,9 @@ if __name__ == '__main__':
     hvd.broadcast_optimizer_state(optimizer, root_rank=0)
 
     batch_size = configs["MODEL"]["batch_size"]
-    fraction = 0.1
+    fraction = 0.2
     seed = 41
-    
+
     epoch_no = 100
     total_duration = 0
 
@@ -474,14 +501,14 @@ if __name__ == '__main__':
 
         validation(epoch)
         if rank==0:
-            print("Iteration {0} took {1:.2f} seconds".format(epoch, duration))
+            #print("Iteration {0} took {1:.2f} seconds".format(epoch, duration))
             plt_train_time.append(duration)
 
         avg_duration = total_duration / epoch_no
-        if rank==0:
-            print("Average iteration duration: {0:.2f} seconds".format(avg_duration))
+        #if rank==0:
+            #print("Average iteration duration: {0:.2f} seconds".format(avg_duration))
         sys.stdout.flush()
- 
+
     # Draw plot
     if rank ==0:
         fig, ax = plt.subplots(3, 1, figsize=(8, 10))

imagenet_nodeComm.py
@@ -1,7 +1,7 @@
 from mpi4py import MPI
 import random
 import numpy as np
-import math 
+import math
 import torch
 import sys
 import pickle
@@ -38,7 +38,7 @@ class ImageNetNodeCommunication:
 
     def _set_send(self, send_request):
         self.send_request = send_request
-    
+
     def _set_recv(self, recv_request):
         self.recv_request = recv_request
 
@@ -48,10 +48,10 @@ class ImageNetNodeCommunication:
 
         shuffle_count = math.floor(len(self.dataset) * self.fraction)
         shuffle_count = torch.tensor(shuffle_count)
-    
+
         min_shuffle_count = hvd.allreduce(shuffle_count, op=hvd.mpi_ops.Min)
         min_shuffle_count = min_shuffle_count.item()
-        
+
         print("Node comm min finding complete. Min shuffle count#{0}".format(min_shuffle_count))
         sys.stdout.flush()
 
@@ -68,28 +68,28 @@ class ImageNetNodeCommunication:
             self.cp_rng.shuffle(self.comm_targets)
             target_rank = self.comm_targets[self.rank]
             target_ranks.append(target_rank)
-        
+
         #self.comm.Barrier()
         hvd.allreduce(torch.tensor(0), name="barrier")
 
         #print("Rank#{0}, Target ranks len:{1}".format(self.rank, len(target_ranks)))
         #sys.stdout.flush()
-        
+
         # Get important sampling
         top_percent = math.ceil(self.fraction * 100) + 3
         important_samples = self.iSample.get_top_x_sample(top_percent) #important samples as a dict
-        
-        if self.rank==0:
-            print("Important Samples: in rank {0}: {1}\n".format(self.rank, important_samples))
-            sys.stdout.flush()
+
+        #if self.rank==0:
+        #    print("Important Samples: in rank {0}: {1}\n".format(self.rank, important_samples))
+        #    sys.stdout.flush()
 
         sample_indexes = list()
         #for idx in range(0, min_shuffle_count):
         for sample_idx, loss in important_samples.items():
             #sample_index = self.permutation[idx]
             sample_indexes.append(sample_idx)
-            if self.rank==0:
-                print("Sending sample index: {0}".format(sample_idx))
+            #if self.rank==0:
+            #    print("Sending sample index: {0}".format(sample_idx))
 
         #self.comm.Barrier()
         hvd.allreduce(torch.tensor(0), name="barrier")
@@ -98,29 +98,29 @@ class ImageNetNodeCommunication:
         sample_batch = list()
         for idx in range(len(target_ranks)):
             data_pack = dict()
-           
+
             data_tup = self.dataset[sample_indexes[idx]]
             data_pack['sample'] = data_tup[0]
             data_pack['label'] = data_tup[1]
             data_pack['path'] = data_tup[2]
 
             sample_batch.append(data_pack)
-       
+
         #self.comm.Barrier()
         hvd.allreduce(torch.tensor(0), name="barrier")
 
         for idx in range(len(target_ranks)):
-            
+
             target_rank = target_ranks[idx]
             if self.rank != target_rank:
                 #print("Rank#{0}, sending Rank:{1}".format(self.rank, target_rank))
                 #sys.stdout.flush()
                 data_pack = sample_batch[idx]
-                
+
                 send_data = {'idx': idx, 'sample': data_pack['sample'], 'label': data_pack['label'], 'path': data_pack['path']}
                 req = self.comm.isend(send_data, dest= target_rank, tag=idx)
-                
-                self.send_requests.append(req)                
+
+                self.send_requests.append(req)
                 self.clean_list.append(sample_indexes[idx])
 
                 status = MPI.Status()
@@ -140,7 +140,7 @@ class ImageNetNodeCommunication:
         if self.send_requests is not None and len(self.send_requests)>0:
             for req in self.send_requests:
                 req.wait()
-        
+
         #self.comm.Barrier()
         hvd.allreduce(torch.tensor(0), name="barrier")
 
@@ -171,7 +171,7 @@ class ImageNetNodeCommunication:
 
         random.seed(self.seed + epoch)
         random.shuffle(self.permutation)
-    
+
     def sync_send(self):
         if self.send_requests is not None and len(self.send_requests) > 0:
             count=0