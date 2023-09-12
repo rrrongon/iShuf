2f5506eb7067da15e217ac34a05bd627a7df8d27
r.rongon <r.rongon@wsu.edu>
2023-09-12 10:11:05 -0700

    -partition.sh

--- imagenet_datapartition.sh
+++ imagenet_datapartition.sh
@@ -3,4 +3,5 @@
 mpirun -np 6 python imagenet_dataPartition.py -npp 6 -f 'imagenet_dataset/imagenet21k_resized/train'
 
 #mpirun -np 4 python imagenet_dataPartition.py -npp 4 -f 'imagenet_dataset/imagenet-mini/train'
-mpirun -np 6 python imagenet_partition_copy.py
+mpirun -np 2 python imagenet_partition_copy.py
+mpirun -np 1 python imagenet_partition_copy.py
--- preprocess.sh
+++ preprocess.sh
@@ -1,6 +1,10 @@
-cd DataProcessing
-./data_partition.sh
-cd ..
-mpirun -n 4 python UnitTest/Test_Partition_download.py
-mpirun -n 4 python utility/_dataSplit.py
-mpirun -n 4 python utility/labeling.py
+#cd DataProcessing
+#./data_partition.sh
+#cd ..
+#mpirun -n 4 python UnitTest/Test_Partition_download.py
+#mpirun -n 4 python utility/_dataSplit.py
+#mpirun -n 4 python utility/labeling.py
+
+#mpirun -np 2 python imagenet_dataPartition.py -npp 2 -f 'imagenet_dataset/imagenet21k_resized/train'
+mpirun -np 1 python imagenet_partition_copy.py
+horovodrun -np 2 -H localhost:2 python dataloader_test.py
